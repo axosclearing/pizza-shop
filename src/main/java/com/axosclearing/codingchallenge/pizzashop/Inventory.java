697a7715776668142680bb0034cfab972a73f03c
ChrisDiep <chrisldiep@gmail.com>
2021-03-01 16:25:52 -0800

    Added inventory class with variables

---
+++ src/main/java/com/axosclearing/codingchallenge/pizzashop/Inventory.java
@@ -0,0 +1,11 @@
+package com.axosclearing.codingchallenge.pizzashop;
+
+public class Inventory {
+    private int cheese;
+    private int pepperoni;
+    private int sausage;
+    private int mushroom;
+    private int pineapples;
+    private int ham;
+    private int jalapeno;
+}
