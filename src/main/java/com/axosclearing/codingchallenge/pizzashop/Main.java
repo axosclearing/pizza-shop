ed173980f754de9e4ebdafcd3f967a0c43da567a
ChrisDiep <chrisldiep@gmail.com>
2021-03-01 17:30:03 -0800

    Added path for reading for reading files

--- src/main/java/com/axosclearing/codingchallenge/pizzashop/Main.java
+++ src/main/java/com/axosclearing/codingchallenge/pizzashop/Main.java
@@ -1,8 +1,16 @@
 package com.axosclearing.codingchallenge.pizzashop;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class Main {
 
     public static void main(String[] args) {
         // TODO implement
+        Path path = Paths.get("./src/main/resources/ingredient_inventory.csv");
+        File file = new File(path.toString());
+        Inventory inventory = new Inventory(file);
     }
 }
